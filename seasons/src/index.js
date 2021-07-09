f905db794235b8762696dc99aab69b2de0e6050a
Guilherme Reis <guilherme.reis@sys10.me>
2021-07-08 21:21:19 -0300

    Conclusão do módulo sobre state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -11,21 +11,60 @@ import ReactDOM from 'react-dom';
 // };
 
 class App extends Component {
+  // o constructor é uma função especial da linguagem do javascript (e não do react)
+  // e ela é sempre a primeira função a ser chamada dentro de um componente.
+  // por isso, devemos iniciar nosso state dentro do nosso construtor.
+  // dentro dele, temos que chamar a função super(props), para certificarmos
+  // que a função implementada na classe Component (que extendemos) seja chamada,
+  // e só após isso inicializemos o nosso state.
   constructor(props) {
-    super(props);
-    this.state = {latitude: ''};
-  }
+    super(props); // super é uma referência a função do parente (pai)
+    this.state = { latitude: null, errorMessage: '' };
 
-  render() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({latitude: position.coords.latitude}),
-      (err) => console.log(err)
-    );
-
-    return (
-      <div> Latitude: {this.state.latitude}</div>
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
+
+  // o react diz que SEMPRE temos que definir nossa função render().
+  render() {
+    // OBS: NUNCA devemos fazer isso de chamar um metódo desse tipo no render(),
+    // porque o metódo render() é chamado diversas vezes no componente. ainda vamos
+    // aprender sobre lifecycles.
+    // window.navigator.geolocation.getCurrentPosition(
+    //   (position) => this.setState({latitude: position.coords.latitude}),
+    //   (err) => console.log(err)
+    // );
+
+    // renderização condicional! renderizamos o componente de acordo com o state
+    // ou de acordo com o props.
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div>Latitude: {this.state.latitude}</div>;
+    }
+
+    return <div>Loading...</div>;
+  }
+
+  // quando o componente é renderizado, essa função é chamada
+  componentDidMount() {
+    console.log("component did mount")
+  }
+
+  // quando o componente é atualizado (renderizado novamente), essa função
+  // é chamada
+  componentDidUpdate() {
+    console.log("component did update")
+  }
+  
 }
 
 ReactDOM.render(
