fd6e7f5ad3875d0b790c9669ac41f9fb4d0d5b4f
Guilherme Reis Araújo <guih2127@gmail.com>
2021-08-19 15:10:35 -0300

    Mais de redux

--- redux/redux-intro.js
+++ redux/redux-intro.js
@@ -30,4 +30,51 @@ const createClaim = (name, amount) => {
       amount: amount
     }
   }
-}
+};
+
+const claimsHistory = (oldListOfClaims = [], action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return [...oldListOfClaims, action.payload]; 
+        // pega uma array e adiciona para uma NOVA array (...)
+        // após isso, adicionamos o novo payload.
+        // evitamos ao máximo modificar uma array existente em um reducer!
+    };
+
+    return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney = 1000, action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return bagOfMoney - action.payload.amount;
+    } else if (action.type === 'CREATE_POLICY') {
+        return bagOfMoney + action.payload.amount
+    };
+
+    return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+        return [...listOfPolicies, action.payload];
+    } else if (action.type === 'DELETE_POLICY') {
+        return listOfPolicies.filter(name => {
+            name !== action.payload.name;
+        });
+    };
+
+    return listOfPolicies;
+};
+
+const { createStore, combineReducers } = Redux;
+
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies
+});
+
+const store = createStore(ourDepartments);
+
+const action = createPolicy('Alex', 20);
+store.dispatch(action);
+console.log(store.getState());
